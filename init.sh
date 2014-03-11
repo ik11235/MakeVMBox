5c798dfacd302d3d1ea97823c319362d5a978848
ik11235 <ik11235@gmail.com>
2014-03-11 16:06:33 +0900

    Make Version-list for veewee

--- init.sh
+++ init.sh
@@ -11,17 +11,33 @@ fi
 #veeweeからOSイメージの一覧を確認、選択したOSのイメージ一覧を表示
 OSLIST=(`veewee vbox templates | cut -d ' ' -f 5 |cut -d '-' -f 1 |grep -v "available" |  sort |uniq |sed -e s/\'//g`)
 echo "What linux distribution do you install?"
-select ANS in ${OSLIST}
+select SELECT_OS in ${OSLIST}
 do
-if [ -z "$ANS" ]; then
+if [ -z "$SELECT_OS" ]; then
     continue
 else
     break
 fi
 done
 
-echo You selected $REPLY\) $ANS
+echo You selected $REPLY\) $SELECT_OS
 
+##Make version list 
+VARLIST=(`veewee vbox templates | grep ${SELECT_OS}| cut -d ' ' -f 5|sed -e s/\'//g|sed -e s/${SELECT_OS}-//g`)
+
+echo "What version do you install?"
+select SELECT_VER in ${VARLIST}
+do
+if [ -z "$SELECT_VER" ]; then
+    continue
+else
+    break
+fi
+done
+
+echo You selected $REPLY\) $SELECT_VER
+
+echo Install image is ${SELECT_OS}-${SELECT_VER}
 
 
 
