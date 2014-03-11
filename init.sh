eba16ddd5eef5f8ab63e2ff1cd15ed5470d3a9c0
ik11235 <ik11235@gmail.com>
2014-03-11 15:56:40 +0900

    Make OSlist for veewee

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@ if [ ! `which ${VIRTUALBOX}` ] || [ ! `which ${VEGRANT}` ]; then
     exit 1;
 fi
 
-OSLIST=("Ubuntu" "CentOS" "Debian" "fedora")
+#veeweeからOSイメージの一覧を確認、選択したOSのイメージ一覧を表示
+OSLIST=(`veewee vbox templates | cut -d ' ' -f 5 |cut -d '-' -f 1 |grep -v "available" |  sort |uniq |sed -e s/\'//g`)
 echo "What linux distribution do you install?"
 select ANS in ${OSLIST}
 do
@@ -20,24 +21,8 @@ fi
 done
 
 echo You selected $REPLY\) $ANS
-echo    
-case "$ANS" in
-    ${OSLIST[1]} ) 
-	#ubuntu
-	;;
-    ${OSLIST[2]} )
-        #CentOS
-	;;
-    ${OSLIST[3]} )
-	#Debian
-	;;
-    ${OSLIST[3]} )
-	#fedora
-	;;
-esac
 
 
-#veeweeからOSイメージの一覧を確認、選択したOSのイメージ一覧を表示
 
 
 #Opscode Public Cookbooksからcehfのcookbookの一覧取得
