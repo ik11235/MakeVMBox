e31079373cec67a0821071e253618a34355da0e5
ik11235 <ik11235@gmail.com>
2014-03-11 14:00:37 +0900

    ADD command found check

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 
+VEGRANT="vagrant"
+VIRTUALBOX="virtualbox"
+
+if [ ! `which ${VIRTUALBOX}` ] || [ ! `which ${VEGRANT}` ]; then
+    echo 'Vegrant or VirtualBox not found'
+    exit 1;
+fi
