7c031313d6574219c83cd832cf72fa0baaa0f9dc
ik11235 <ik11235@gmail.com>
2014-03-11 14:16:46 +0900

    ADD select OS message

--- init.sh
+++ init.sh
@@ -7,3 +7,37 @@ if [ ! `which ${VIRTUALBOX}` ] || [ ! `which ${VEGRANT}` ]; then
     echo 'Vegrant or VirtualBox not found'
     exit 1;
 fi
+
+OSLIST=("Ubuntu" "CentOS" "Debian" "fedora")
+echo "What linux distribution do you install?"
+select ANS in ${OSLIST}
+do
+if [ -z "$ANS" ]; then
+    continue
+else
+    break
+fi
+done
+
+echo You selected $REPLY\) $ANS
+echo    
+case "$ANS" in
+    ${OSLIST[1]} ) 
+	#ubuntu
+	;;
+    ${OSLIST[2]} )
+        #CentOS
+	;;
+    ${OSLIST[3]} )
+	#Debian
+	;;
+    ${OSLIST[3]} )
+	#fedora
+	;;
+esac
+
+
+#veeweeからOSイメージの一覧を確認、選択したOSのイメージ一覧を表示
+
+
+#Opscode Public Cookbooksからcehfのcookbookの一覧取得
